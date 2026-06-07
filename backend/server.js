f56e66177cfa50167764927d344fd4d24435ec21
vai-shnavi21 <vkale4054@gmail.com>
2026-06-07 14:16:02 +0530

    Added root route

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.get('/', (req, res) => {
+  res.send('Employee Management Backend Running');
+});
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
