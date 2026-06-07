0ffa46f0769153e6e260ee5f101b8cf1b03622dd
vai-shnavi21 <vkale4054@gmail.com>
2026-06-07 14:28:39 +0530

    Connect frontend to Render backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({
+  baseURL: 'https://employee-management-backend-yx99.onrender.com/api'
+});
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
